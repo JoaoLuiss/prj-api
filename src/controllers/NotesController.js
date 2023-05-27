e6b5cb7cb22e01121985911c4ec08e296070f614
JoaoLuiss <joaoluisseguro@gmail.com>
2023-05-27 19:57:40 -0300

    Ajuste devido a uma atualização do Knex

--- src/controllers/NotesController.js
+++ src/controllers/NotesController.js
@@ -4,7 +4,7 @@ class NotesController {
 	async create(request, response) {
 		const { title, description, tags, links } = request.body;
 		const user_id = request.user.id;
-		const note_id = await knex('notes').insert({
+		const [note_id] = await knex('notes').insert({
 			title,
 			description,
 			user_id
