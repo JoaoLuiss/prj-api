f40e5f464c2d2f03aa365378e620da5e898d0f64
JoaoLuiss <joaoluisseguro@gmail.com>
2023-06-03 16:53:18 -0300

    Implementando módulos e ajustes para o Deploy
    Instalação e utilização de variáveis de ambiente (environment) com 'dotenv'
    
    Instalação e utilização do módulo 'PM2' que garante a reinicialização do servidor no caso de crash para garantir 24 horas de disponibilidade.

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 require('express-async-errors');
+require('dotenv/config');
 const migrationRun = require('./database/sqlite/migrations');
 const AppError = require('./utils/AppError');
 const uploadConfig = require('./configs/upload');
@@ -15,22 +16,22 @@ app.use(express.json());
 app.use('/files', express.static(uploadConfig.UPLOADS_FOLDER));
 app.use(routes);
 app.use(function (error, req, resp, next) {
-  if (error instanceof AppError) {
-    return resp.status(error.statusCode).json({
-      status: 'error',
-      message: error.message
-    });
-  }
+	if (error instanceof AppError) {
+		return resp.status(error.statusCode).json({
+			status: 'error',
+			message: error.message
+		});
+	}
 
-  console.error(error);
+	console.error(error);
 
-  return response.status(500).json({
-    status: 'error',
-    message: 'Internal server erro'
-  });
+	return response.status(500).json({
+		status: 'error',
+		message: 'Internal server erro'
+	});
 });
 
-const PORT = 3333;
+const PORT = process.env.SERVER_PORT || 3333;
 app.listen(PORT, function (req, res) {
-	console.log(`Server is running on port: ${PORT}\nhttp://localhost:3333`);
+	console.log(`Server is running on port: ${PORT} -> http://localhost:${PORT}`);
 });
